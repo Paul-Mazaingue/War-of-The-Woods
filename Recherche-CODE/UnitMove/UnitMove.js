f9cf3136ef62687f0e9a6ccfe1cb39b4e01338fa
AdamVidal <90256804+AdamVidal@users.noreply.github.com>
2023-03-17 18:06:56 +0100

    Finalisation de l'animation et prise en compte des limites de la map lors des déplacements

--- Recherche-CODE/UnitMove/UnitMove.js
+++ Recherche-CODE/UnitMove/UnitMove.js
@@ -3,10 +3,10 @@ const matrice_cases = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
@@ -26,20 +26,47 @@ const matrice_unites = [
 
 const liste_unites = [];
 
+
+const gridContainer = document.getElementById("grid-container");
+const gridStyle = window.getComputedStyle(gridContainer);
+const gridWidth = parseInt(gridStyle.width);
+const gridHeight = parseInt(gridStyle.height);
+const square_size = 50;
+const gridLeft = parseInt(gridStyle.left);
+const gridTop = parseInt(gridStyle.top);
+
 class Unite{
 
-    constructor (x = null,y = null,hitbox_radius = 0){
+    constructor (x = null,y = null,hitbox_radius = 0, imagesrc = ""){
         this.x = x;
         this.y = y;
         this.hitbox_radius = hitbox_radius;
         liste_unites.push(this);
+        this.imagesrc = imagesrc;
+        this.image = document.createElement("img");
+        this.image.setAttribute('src', this.imagesrc);
+        document.body.appendChild(this.image);
+        this.imgStyle = window.getComputedStyle(this.image);
+        this.imgWidth = parseInt(this.imgStyle.width);
+        this.imgHeight = parseInt(this.imgStyle.height);
+        this.image.style.left = `${gridLeft}px`;
+        this.image.style.top = `${gridTop}px`; 
+        this.image.style.animation = 'move-image 1s forwards';
+        this.image.animate([
+            { transform: 'translate(0,0)' },
+            { transform: 'translate('+(this.x*square_size)+'px,'+(this.y*square_size)+'px)' }
+          ], {
+            duration: 0,
+            fill: "forwards"
+          })
+        this.image.style.animation = 'none';
     }
 }
 
 x_test = 2;
 y_test = 3;
-unite_test = new Unite(x_test,y_test);
-console.log(unite_test.x,unite_test.y,unite_test.hitbox_radius);
+unite_test = new Unite(x_test,y_test, 0, "unit.png");
+console.log(unite_test.x, unite_test.y, unite_test.hitbox_radius);
 
 
 liste_objet_id = matrice_unites[y_test][x_test];
@@ -53,9 +80,8 @@ if (liste_objet_id[0]==1){
 
 
 // Sélection de l'image à déplacer
-const image = document.querySelector('img');
-image.style.left = `${x_test*50 - parseInt(window.getComputedStyle(image).width) + 50 + parseInt(window.getComputedStyle(document.getElementById("grid-container")).left)}px`;
-image.style.top = `${y_test*50 - parseInt(window.getComputedStyle(image).height) + 50 + parseInt(window.getComputedStyle(document.getElementById("grid-container")).top)}px`;
+const image = liste_unites[liste_objet_id[1]].image;
+
 
 // Variables pour stocker la position de l'image et le mode de déplacement
 let posImageX, posImageY, isMoving = false;
@@ -65,7 +91,7 @@ function onImageClick(event) {
   console.log("img click");
   if (isMoving) {
     // Si le mode de déplacement est activé, on calcule la nouvelle position de l'image
-    let x = getCoords()[0]*square_size;
+    /*let x = getCoords()[0]*square_size;
     let y = getCoords()[1]*square_size;
     let imgStyle = window.getComputedStyle(image);
     let imgWidth = parseInt(imgStyle.width);
@@ -75,7 +101,7 @@ function onImageClick(event) {
     console.log("x",x,"y",y,"w",imgWidth,"h",imgHeight);
     image.style.left = `${x - imgWidth + square_size + gridLeft}px`;
     image.style.top = `${y - imgHeight + square_size + gridTop}px`;
-    isMoving = false;
+    isMoving = false;*/
   } else {
     // Si le mode de déplacement n'est pas activé, on stocke la position de l'image
     posImageX = image.offsetLeft;
@@ -95,44 +121,44 @@ function getCoords() {
   return [Math.floor((event.clientX-gridLeft)/square_size), Math.floor((event.clientY-gridTop)/square_size)];
 }
 
+function movementAnimationUnit(unit,destination_x,destination_y,movement_duration){
+  unit.image.style.animation = 'move-image 1s forwards';
+  unit.image.animate([
+      { transform: 'translate('+(unit.x*square_size)+'px,'+(unit.y*square_size)+'px)' },
+      { transform: 'translate('+(destination_x*square_size)+'px,'+(destination_y*square_size)+'px)' }
+    ], {
+      duration: movement_duration,
+      fill: "forwards"
+    })
+  unit.image.style.animation = 'none';
+}
+
 // Fonction qui sera exécutée lorsque l'utilisateur clique n'importe où sur la page
 function onPageClick(event) {
   console.log("page click");
   console.log("x:",getCoords()[0],"y:",getCoords()[1]);
   let destination_x = getCoords()[0];
   let destination_y = getCoords()[1];
-  let x = destination_x*square_size;
-  let y = destination_y*square_size;
-  console.log("x2:",x,"y2:",y);
-  if (isMoving) {
-    // Si le mode de déplacement est activé, on calcule la nouvelle position de l'image
-    let imgStyle = window.getComputedStyle(image);
-    let imgWidth = parseInt(imgStyle.width);
-    let imgHeight = parseInt(imgStyle.height);
-    let gridLeft = parseInt(gridStyle.left);
-    let gridTop = parseInt(gridStyle.top);
-    console.log("x",x,"y",y,"w",imgWidth,"h",imgHeight);
-    matrice_unites[unite_test.y][unite_test.x]=null;
-    console.log(unite_test.x,destination_x,destination_x*square_size-unite_test.x*square_size);
-    console.log(unite_test.y,destination_y,destination_y*square_size-unite_test.y*square_size);
-    image.style.animation = 'move-image 1s forwards';
-    image.animate([
-        { transform: 'translate(0,0)' },
-        { transform: 'translate('+(destination_x*square_size-unite_test.x*square_size)+'px,'+(destination_y*square_size-unite_test.y*square_size)+'px)' }
-      ], {
-        duration: 1000,
-        fill: "forwards"
-      })
-    image.style.animation = 'none';
-    const t = setTimeout(() => {
-        image.style.left = `${x - imgWidth + square_size + gridLeft}px`;
-        image.style.top = `${y - imgHeight + square_size + gridTop}px`;
-    }, 1_000)
-    unite_test.x=destination_x;
-    unite_test.y=destination_y;
-    matrice_unites[unite_test.y][unite_test.x]=[1,0];
-    console.log(matrice_unites);
-    isMoving = false;
+  if(matrice_cases[destination_y][destination_x]==1){
+    let x = destination_x*square_size;
+    let y = destination_y*square_size;
+    console.log("x2:",x,"y2:",y);
+    if (isMoving) {
+      // Si le mode de déplacement est activé, on calcule la nouvelle position de l'image
+      let imgStyle = window.getComputedStyle(image);
+      let imgWidth = parseInt(imgStyle.width);
+      let imgHeight = parseInt(imgStyle.height);
+      console.log("x",x,"y",y,"w",imgWidth,"h",imgHeight);
+      matrice_unites[unite_test.y][unite_test.x]=null;
+      console.log(unite_test.x,destination_x,destination_x*square_size-unite_test.x*square_size);
+      console.log(unite_test.y,destination_y,destination_y*square_size-unite_test.y*square_size);
+      movementAnimationUnit(unite_test,destination_x,destination_y, 1000);
+      unite_test.x=destination_x;
+      unite_test.y=destination_y;
+      matrice_unites[unite_test.y][unite_test.x]=[1,0];
+      console.log(matrice_unites);
+      isMoving = false;
+    }
   }
 }
 
@@ -170,11 +196,6 @@ function drawGrid(gridWidth, gridHeight, squareSize) {
 // Appel de la fonction drawGrid avec un paramètre gridSize de 10 (par exemple)
 /*const pageWidth = window.innerWidth;
 const pageHeight = window.innerHeight;*/
-const gridContainer = document.getElementById("grid-container");
-const gridStyle = window.getComputedStyle(gridContainer);
-const gridWidth = parseInt(gridStyle.width);
-const gridHeight = parseInt(gridStyle.height);
-const square_size = 50;
 console.log(gridWidth,gridHeight,square_size);
 console.log(Math.floor(gridWidth/square_size), Math.floor(gridHeight/square_size), square_size);
 drawGrid(Math.floor(gridWidth/square_size), Math.floor(gridHeight/square_size), square_size);
