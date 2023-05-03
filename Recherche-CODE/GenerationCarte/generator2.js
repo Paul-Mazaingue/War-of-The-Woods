9bfe354441fc804f87087a44ed2d7b296e18a455
Paul-Mazaingue <90213396+Paul-Mazaingue@users.noreply.github.com>
2023-05-03 21:42:21 +0200

    avancement fonction playzone : Création des points de supports, reliage de ces points pour former la bordure, remplissage de l'intérieur de la bordure (encore un bug à ce niveau là)

--- Recherche-CODE/GenerationCarte/generator2.js
+++ Recherche-CODE/GenerationCarte/generator2.js
@@ -31,11 +31,94 @@ class MapGenerator {
 
 
         const points = [];
-        //for(let i = 0; i < ; i++) {
-        
+        const distMin = Math.round(Math.round(this.width/2) * 0.3);
+        const distMax = Math.round(Math.round(this.width/2) * 0.7);
+        // Il faudra modifier le * 0.5 par l'indicateur influant sur la taille de la zone de jeu 
+        const bonusIndicator = Math.round(Math.round(this.width/2) * 0.25) * 0;
+
+
+        const position = [[0,-1], [1,-1], [1,0], [1,1], [0,1], [-1,1], [-1,0], [-1,-1]];
+
+        position.forEach(pos => {
+            let dist = Math.round(Math.random() * (distMax - distMin) + distMin + bonusIndicator);
+            points.push([pos[0] * (dist) + x, pos[1] * (dist) + y]);
+        });
+
+
+        this.linkPoints(points);
+        this.fillEarth();
+        console.log(points);
 
         console.log("temps d'éxécution playZone : " + (Date.now() - now) + "ms");
         
+    }
+
+    linkPoints(points) {
+
+        for(let i = 0; i < points.length; i++) {
+            let pointsStart = points[i];
+            let pointsEnd;
+            if(i == points.length - 1) {
+                pointsEnd = points[0];
+            } else {
+                pointsEnd = points[i + 1];
+            }
+
+            let x = pointsStart[0];
+            let y = pointsStart[1];
+
+            const addX = pointsEnd[0] - pointsStart[0] > 0 ? 1 : -1;
+            const addY = pointsEnd[1] - pointsStart[1] > 0 ? 1 : -1;
+
+            const distX = Math.abs(pointsEnd[0] - pointsStart[0]);
+            const distY = Math.abs(pointsEnd[1] - pointsStart[1]);
+
+            const ratio = distX / (distX + distY);
+
+
+            
+
+            while(x != pointsEnd[0] || y != pointsEnd[1]) {
+                if ( x != pointsEnd[0] && y != pointsEnd[1]) {
+                    if(Math.random() > ratio) {
+                        y += addY;
+                        
+                    } else {
+                        x += addX;
+                    }
+                }
+                else if (x != pointsEnd[0]) {
+                    x += addX;
+                }
+                else if (y != pointsEnd[1]) {
+                    y += addY;
+                }
+
+                this.unitsElementsMatrix[y][x] = 0;
+            }
+
+        }
+            
+    }
+
+    fillEarth() {
+        for(let y = 0; y < this.height; y++) {
+            let fill = false;
+            for(let x = 0; x < this.width-1; x++) {
+                //console.log(fill)
+                if(fill) {
+                    this.unitsElementsMatrix[y][x] = 0;
+                }
+                if(this.unitsElementsMatrix[y][x+1] == 0 && this.unitsElementsMatrix[y][x+2] == -1) {
+                    fill = !fill;
+                    x += 2;
+                    if(fill) {  
+                        this.unitsElementsMatrix[y][x] = 0;
+                    }
+                }
+                
+            }
+        }
     }
 
 
@@ -85,7 +168,18 @@ class MapGenerator {
                 const value = this.unitsElementsMatrix[x][y];
 
                 // Set color based on the value (0 for black)
-                ctx.fillStyle = value === -1 ? "blue" : "brown";
+                switch (value) { 
+                    case -1:
+                        ctx.fillStyle = "blue";
+                        break;
+                    case 0:
+                        ctx.fillStyle = "brown";
+                        break;
+                    case 1:
+                        ctx.fillStyle = "black";
+                        break;
+                }
+                //ctx.fillStyle = value === -1 ? "blue" : "brown";
 
                 // Draw the pixel
                 ctx.fillRect(x, y, 1, 1);
