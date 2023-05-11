c449b8efc3f72709e3a47213e6e5c0e773a52994
Paul-Mazaingue <90213396+Paul-Mazaingue@users.noreply.github.com>
2023-05-11 13:24:48 +0200

    ajout d'une vérification d'un chemin existant pour les mines et les pack d'ennemies

--- Recherche-CODE/Jeu/load.js
+++ Recherche-CODE/Jeu/load.js
@@ -28,6 +28,54 @@ class Load {
         [0  , 101, 101, 101, 0],
         [0  , 0  , 0  , 0  , 0]]
     ]
+
+    static EnnemieConfig = [[
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 300  , 300  , 0, 0],
+        [0  , 0  , 200, 300  , 300  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0]],
+
+        [
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 300  , 300  , 0, 0],
+        [0  , 0  , 200, 300  , 300  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0]],
+
+        [
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 300  , 300  , 0, 0],
+        [0  , 0  , 200, 300  , 300  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0]],
+
+        [
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 300  , 300  , 0, 0],
+        [0  , 0  , 200, 300  , 300  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0]],
+        
+        [
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 300  , 300, 0],
+        [0  , 0  , 200, 0  , 300  , 300, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0],
+        [0  , 0  , 200, 0  , 0  , 0, 0]]
+
+
+    ]
     
     static ennemies = [200, 201] 
 
@@ -35,12 +83,16 @@ class Load {
         const now = Date.now();
         for (let y = 0; y < this.unitElement.length; y++) {
             for (let x = 0; x < this.unitElement[0].length; x++) {
-                if([6].includes(this.unitElement[y][x])) {
+                if([6,7].includes(this.unitElement[y][x])) {
 
                     switch (this.unitElement[y][x]) {
                         case 6:
                             this.placetotem(x, y);
-                            
+                            break;
+                        case 7:
+                            this.placeEnnemies(x, y);
+                            break;
+
                     }
                 }
             }
@@ -55,12 +107,26 @@ class Load {
                 let value = Load.totemConfig[config][i][j];
                 if(value == 200) {
                     value = Load.ennemies[Math.round(Math.random() * (Load.ennemies.length - 1))];
-                    console.log(value)
                 }
                 this.unitElement[y+i][x+j] = value;
             }
         }
     }
+
+    placeEnnemies(x,y) {
+        const config = Math.round(Math.random() * (Load.EnnemieConfig.length - 1));
+        for (let i = 0; i < Load.EnnemieConfig[0].length; i++) {
+            for (let j = 0; j < Load.EnnemieConfig[0].length; j++) {
+                let value = Load.EnnemieConfig[config][i][j];
+                if(value == 200) {
+                    value = Load.ennemies[Math.round(Math.random() * (Load.ennemies.length - 1))];  
+                }
+                this.unitElement[y+i][x+j] = value;
+            }
+        }
+
+    }
+
 
     
 }
